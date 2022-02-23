a38daa3e53c65d178bc9fbab790653a6eb135953
ilyass elarbaouti <mrfixer59@gmail.com>
2022-02-23 17:27:21 +0200

    saga

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,11 +1,16 @@
-/* eslint-disable no-underscore-dangle */
-import { createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers/index';
 
-const composeEnhancer =
-  (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-
-const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)));
+const sagaMiddleware = createSagaMiddleware();
+function* rootSaga() {
+  yield console.log('hello');
+}
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+sagaMiddleware.run(rootSaga);
 
 export default store;
